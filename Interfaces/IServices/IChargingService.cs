bcac0262299eb36186d32148c23078acadfc1028
nhatanh <anhnguyenphamnhat@gmail.com>
2025-11-02 17:09:42 +0700

    Update validate scan function for walk-in driver

--- Interfaces/IServices/IChargingService.cs
+++ Interfaces/IServices/IChargingService.cs
@@ -7,7 +7,7 @@ namespace API.Interfaces
 {
     public interface IChargingService
     {
-        Task<(bool CanStart, string Message)> ValidateScanAsync(int postId, string driverId);
+        Task<(bool CanStart, string Message, int? ReservationId, int? VehicleId)> ValidateScanAsync(int postId, string driverId);
         Task StartChargingAsync(string chargerId);
         Task StopChargingAsync(string chargerId);
     }
