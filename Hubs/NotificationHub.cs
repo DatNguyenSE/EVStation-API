7252a69faf0535fb6e12fe42cadf37bf6d865e14
Luong Tuan Kiet <kietboy8@gmail.com>
2025-11-19 23:03:17 +0700

    add chatbot

--- Hubs/NotificationHub.cs
+++ Hubs/NotificationHub.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Helpers;
 using API.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace API.Hubs
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Authorize] // Yêu cầu xác thực (đăng nhập) để kết nối vào Hub này
     public class NotificationHub : Hub<INotificationClient>
     {
